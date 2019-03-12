bebd3ee848b1293166c1d597612cf850c1b94703
Jun Young King <33975124+MAR1026@users.noreply.github.com>
2019-03-12 10:13:55 +0900

    initialize를 가장한 선생님의 명령

---
+++ src/main/java/kr/hs/dgsw/calc/CalcApplication.java
@@ -0,0 +1,15 @@
+package kr.hs.dgsw.calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcApplication.class, args);
+    }
+
+}
+
+// cfb0813150df5c3db4020af24cddd0479ee44d66
